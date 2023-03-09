5745ed96df6a91eeb9b68990bf8b9f29f346677e
Isuru <isurudulakshana012@gmail.com>
2023-03-09 19:42:37 +0530

    Demo-spa-app

---
+++ js/main.js
@@ -0,0 +1,39 @@
+
+hideContent();
+
+function hideContent() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+    document.getElementById('home').style.display="block";
+}
+
+
+function showHomeContent() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+    document.getElementById('home').style.display="block";
+}
+
+
+
+function showSalesContent() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+    document.getElementById('home').style.display="none";
+    document.getElementById('sales').style.display = "block";
+}
+function showAboutContent() {
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+    document.getElementById('home').style.display="none";
+    document.getElementById('about').style.display = "block";
+}
+
+function showContactContent() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('sales').style.display = "none";
+    document.getElementById('home').style.display="none";
+    document.getElementById('contact').style.display = "block";
+}
